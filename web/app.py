8d126d59182bdc94125cf84c12565419f50e5bb0
Syed Mahbubur Rahman <mahbubur.rahman.64@gmail.com>
2022-06-16 22:14:13 +0600

    Changed docker-compose version from 3.9 to 3.3

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
-import json, bcrypt
+import json, bcrypt1
 
 app = Flask(__name__)
 api = Api(app)
