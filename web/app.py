820802e29ef0b751a5a320a7a77bdbc2a1e8ea90
Syed Mahbubur Rahman <mahbubur.rahman.64@gmail.com>
2022-06-16 23:00:54 +0600

    Corrected typo in app.py, changed bcrypt1 to bcrypt

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
-import json, bcrypt1
+import json, bcrypt
 
 app = Flask(__name__)
 api = Api(app)
